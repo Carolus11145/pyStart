41d507ba5e45589c34b3ea46ae88cef298447a65
Carolus11145 <blumkarl221@gmail.com>
2024-09-07 15:56:12 +0200

    Added function for calculating diagonal of rectangle

--- pyStart/4-area_calculator/main.py
+++ pyStart/4-area_calculator/main.py
@@ -10,12 +10,21 @@ class Rectangle:
     def set_w(self, new_w): # sets the width to a different value
         self.w = new_w
         
-    # 3. Create a method which calculates the area of the rectangle
+    # 3. Create two methods which calculate the area and perimeter of the rectangle
     def area(self):
-        area = self.w * self.h
-        return area
+        a = self.w * self.h
+        return a
     
-
+    def perimeter(self):
+        p = (self.w * 2) + (self.h * 2)
+        return p
+    
+    # 4. Create a method which calculates the diagonal of the rectangle
+    def diag(self):
+        d = (self.w**2 + self.h**2) ** .5
+        return d
+    
 quad1 = Rectangle(7, 3)
 quad1.set_h(5)
-print(quad1.area())
+quad1.set_w(10)
+print(quad1.diag())
