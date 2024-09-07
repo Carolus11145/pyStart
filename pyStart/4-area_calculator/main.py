f7462e6c9f58bc73650b9a3cf57fc1ce87a244e7
Carolus11145 <blumkarl221@gmail.com>
2024-09-07 15:43:40 +0200

    Added set_h and set_h

--- pyStart/4-area_calculator/main.py
+++ pyStart/4-area_calculator/main.py
@@ -1,7 +1,16 @@
 class Rectangle:
-    def __init__(self, height, width):
-        self.height = None
-        self.width = None
+    # 1. Initialise height and width attributes of the rectangle class
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h 
+    # 2. Create the two functions for redefining the height and width
+    def set_h(self, new_h): # sets height to a different value
+        self.h = new_h
     
-    def set_width():
-        pass
+    def set_w(self, new_w): # sets the width to a different value
+        self.w = new_w
+        
+    # 3. Create a method which calculates the area of the rectangle
+
+quad1 = Rectangle(7, 3)
+print(quad1.set_h(10))
