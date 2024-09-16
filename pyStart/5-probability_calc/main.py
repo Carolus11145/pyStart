f16c88ee6d43a63349770c42e5776f5c235823e0
Carolus11145 <blumkarl221@gmail.com>
2024-09-16 12:58:42 +0200

    Updated draw function; added print calls

--- pyStart/5-probability_calc/main.py
+++ pyStart/5-probability_calc/main.py
@@ -4,29 +4,31 @@ import random # a module which is composed of methods for generating random nums
 class Hat:
     # 1. Instantiate an instance variable for the contents of the hat along with a dictionary for user inputs
     def __init__(self, **inputs):
-        self.contents = []
-        for n, m in inputs.items():
-            for p in range(m):
-                self.contents.append(n)
+        self.contents = list()
+        
+        for input in inputs:
+            for i in range(inputs[input]):
+                self.contents.append(input)
 
     # 2. Define a method for drawing a number of balls
     def draw(self, num):
-        if len(self.contents) <= num:
+        if len(self.contents) < num:
             return self.contents
-        elif len(self.content) >= num:
-            drawn_items = []
+        
+        elif len(self.contents) >= num:
+            items = list()
 
-            for x in range(num):
+            for x in range(0, num):
             
                 ball = random.choice(self.contents) # 3. Select a random ball from contents
-                drawn_items.append(ball) # 4. Add random ball to the list of drawn items
+                items.append(ball) # 4. Add random ball to the list of drawn items
                 self.contents.remove(ball) # 5. Finally, remove the ball from the list of drawn contents
             
-            return drawn_items
+            return items
         
 # 6. Design a function which uses an instance of the class to return a probability
 def experiment(hat, expected, num_drawn, num_of_experiments):
-    draw_count = 0 # 7. Times that the expected ball is to be drawn
+    last_count = 0 # 7. Times that the expected ball is to be drawn
 
     for x in range(num_of_experiments):
         copy_of_hat = copy.deepcopy(hat) # 8. Start each call with a new, separate hat instance
@@ -38,9 +40,22 @@ def experiment(hat, expected, num_drawn, num_of_experiments):
             if list_of_drawn.count(n) < m:
                 corr = False
                 break
+        
         if corr:
-            draw_count += 1
+            last_count += 1
 
-    fin = draw_count / num_of_experiments
+    fin = last_count / num_of_experiments
 
     return fin
+
+hat1 = Hat(white=4, black=2, yellow=3)
+print(hat1.draw(4))
+print(hat1.draw(5))
+exp = experiment(
+    hat=hat1,
+    expected={'red': 1, 'green': 2},
+    num_drawn=5,
+    num_of_experiments=100
+)
+
+print(exp)
