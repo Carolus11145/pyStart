3725aa84d05f39e3b9efcb139eb2d5067b7223c3
Carolus11145 <blumkarl221@gmail.com>
2024-09-16 15:32:47 +0200

    General updates

--- pyStart/5-probability_calc/main.py
+++ pyStart/5-probability_calc/main.py
@@ -2,60 +2,58 @@ import copy # a module containing methods related to copying different lists, ob
 import random # a module which is composed of methods for generating random nums in various ways
 
 class Hat:
-    # 1. Instantiate an instance variable for the contents of the hat along with a dictionary for user inputs
+    # Instantiate an instance variable for the contents of the hat along with a dictionary for user inputs
     def __init__(self, **inputs):
         self.contents = list()
         
         for input in inputs:
-            for i in range(inputs[input]):
+            for _ in range(inputs[input]):
                 self.contents.append(input)
 
-    # 2. Define a method for drawing a number of balls
+    # Define a method for drawing a number of balls
     def draw(self, num):
-        if len(self.contents) < num:
-            return self.contents
+        if len(self.contents) <= num:
+            return self.contents[:]
         
-        elif len(self.contents) >= num:
-            items = list()
+        items = list()
 
-            for x in range(0, num):
+        for _ in range(0, num):    
+            ball = random.randint(0, len(self.contents) - 1) # Select a random ball from contents
+            items.append(self.contents.pop(ball)) # Add random ball to the list of drawn items
+            # Finally, remove the ball from the list of drawn contents
             
-                ball = random.choice(self.contents) # 3. Select a random ball from contents
-                items.append(ball) # 4. Add random ball to the list of drawn items
-                self.contents.remove(ball) # 5. Finally, remove the ball from the list of drawn contents
-            
-            return items
+        return items
         
-# 6. Design a function which uses an instance of the class to return a probability
-def experiment(hat, expected, num_drawn, num_of_experiments):
-    last_count = 0 # 7. Times that the expected ball is to be drawn
+# Design a function which uses an instance of the class to return a probability
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    origin_hat = copy.copy(hat)
+    last_count = 0 # Times that the expected ball is to be drawn
 
-    for x in range(num_of_experiments):
-        copy_of_hat = copy.deepcopy(hat) # 8. Start each call with a new, separate hat instance
-        list_of_drawn = copy_of_hat.draw(num_drawn) # 9. List of drawn balls
-        corr = True # 10. Boolean val to check the correlation of list of drawn balls 
+    for _ in range(num_experiments):
+        copy_of_hat = copy.deepcopy(origin_hat) # Start each call with a new, separate hat instance
+        list_of_drawn = copy_of_hat.draw(num_balls_drawn) # List of drawn balls
+        corr = True # Boolean val to check the correlation of list of drawn balls 
         
-        for n, m in expected.items():
-            # 11. Iterate through the key-val pairs to check whether not the corr bool is true
+        for n, m in expected_balls.items():
+            # Iterate through the key-val pairs to check whether not the corr bool is true
             if list_of_drawn.count(n) < m:
                 corr = False
                 break
         
         if corr:
             last_count += 1
-
-    fin = last_count / num_of_experiments
-
-    return fin
+    # Final return value of the experiment function
+    return last_count / num_experiments
 
 hat1 = Hat(white=4, black=2, yellow=3)
-print(hat1.draw(4))
-print(hat1.draw(5))
+hat2 = Hat(blue=2, green=5, red=4)
+hat3 = Hat(orange=6, pink=2, purple=3, brown=5, striped=3)
+print(hat3.draw(8))
 exp = experiment(
-    hat=hat1,
-    expected={'red': 1, 'green': 2},
-    num_drawn=5,
-    num_of_experiments=100
+    hat=hat3,
+    expected_balls={'orange': 1, 'brown': 3},
+    num_balls_drawn=5,
+    num_experiments=1000
 )
 
 print(exp)
