4de8aa146bb317bc5a6be36ead5bd23c673f466f
Carolus11145 <blumkarl221@gmail.com>
2024-09-18 11:36:30 +0200

    Added conditionals and dict for calculations

--- pyData/mean-var-calculator/mean_var_std.py
+++ pyData/mean-var-calculator/mean_var_std.py
@@ -1,8 +1,15 @@
-import numpy as np
+import numpy as np # The calculator must be completed using numpy methods
 
 def calculate(list):
-    pass
+    # Conditional that checks if the list's length is more than nine
+    if (len(list) > 9):
+        raise ValueError('List must contain nine numbers.')
 
+    if isinstance(list, np.ndarray):
+        raise TypeError('Input must be a python list.')
+    
+    calculations = {
+        
+    }
 
-
-    # return calculations
+    return calculations
