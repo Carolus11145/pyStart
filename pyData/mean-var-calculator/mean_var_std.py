ee106aed5c7d3831956e589b06728d5b633ba51e
Carolus11145 <blumkarl221@gmail.com>
2024-09-18 16:34:17 +0200

    Completed calculate function

--- pyData/mean-var-calculator/mean_var_std.py
+++ pyData/mean-var-calculator/mean_var_std.py
@@ -1,15 +1,22 @@
 import numpy as np # The calculator must be completed using numpy methods
 
-def calculate(list):
+def calculate(arr):
     # Conditional that checks if the list's length is more than nine
-    if (len(list) > 9):
+    if len(arr) != 9:
         raise ValueError('List must contain nine numbers.')
-
-    if isinstance(list, np.ndarray):
+    # Conditional for verifying whether or not the list is numpy array
+    if isinstance(arr, np.ndarray):
         raise TypeError('Input must be a python list.')
-    
-    calculations = {
-        
+    # The matrix which will hold the 3x3 numpy array
+    matrix = np.array(arr).reshape(3, 3)
+    # The dict which will store the calculated statisitics
+    fin = {
+        'mean': [list(matrix.mean(axis=0)), list(matrix.mean(axis=1)), matrix.mean()],
+        'variance': [list(matrix.var(axis=0)), list(matrix.var(axis=1)), matrix.var()],
+        'standard deviation': [list(matrix.std(axis=0)), list(matrix.std(axis=1)), matrix.std()],
+        'max': [list(matrix.max(axis=0)), list(matrix.max(axis=1)), matrix.max()],
+        'min': [list(matrix.min(axis=0)), list(matrix.min(axis=1)), matrix.min()],
+        'sum': [list(matrix.sum(axis=0)), list(matrix.sum(axis=1)), matrix.sum()]
     }
 
-    return calculations
+    return fin
