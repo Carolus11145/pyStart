ea23e3a209aaca95c9cedada7115b2583fe2066a
Carolus11145 <blumkarl221@gmail.com>
2024-09-19 12:50:12 +0200

    Finished demographic data function

--- pyData/demographic_analyser/demographic_data_analyzer.py
+++ pyData/demographic_analyser/demographic_data_analyzer.py
@@ -1,46 +1,46 @@
 import pandas as pd
 
-
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult_data.csv')
 
-    # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    # Panda series with race names as the index labels:
+    race_count = df['race'].value_counts()
 
-    # What is the average age of men?
-    average_age_men = None
+    # The average age of men in the data set:
+    average_age_men = round(df[df['sex'] == 'Male']['age'].mean(), 1)
 
-    # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    # Percentage of people in the data set who have a Bachelor's Degree:
+    percentage_bachelors = round(df[df['education'] == 'Bachelors'].shape[0] / df .shape[0] * 100, 1)
 
-    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
-    # What percentage of people without advanced education make more than 50K?
+    # Percentage of people with advanced education who make more than 50K:
+    # Percentage of people without advanced education who make more than 50K:
 
-    # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    query1 = df['education'].isin(['Bachelors', 'Masters', 'Doctorate']) # Records those people who have an advanced education
+    query2 = df['salary'] == '>50k' # Records those people who do not have an advanaced education
 
-    # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = round((query1 & query2).sum() / query1.sum() * 100, 1)
+    lower_education_rich = round((~query1 & query2).sum() / (~query1).sum() * 100, 1)
 
-    # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    # Minimum number of hours a person works per week:
+    min_work_hours = df['hours-per-week'].min()
 
-    # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    # Percentage of people who work the minimum number of hours per week and have a salary of >50K:
+    query3 = df['hours-per-week'] == min_work_hours # Tracks people in the data set that 
 
-    rich_percentage = None
+    rich_percentage = round((query3 & query2).sum() / query3.sum() * 100, 1)
 
-    # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    # Country with the highest percentage of people that earn >50K:
+    query4 = (df[query2]['native-country'].value_counts() \
+              / df['native-country'].value_counts() * 100).sort_values(ascending=False)
+    highest_earning_country = query4.index[0]
+    highest_earning_country_percentage = round(query4.iloc[0], 1)
 
-    # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    # Most popular occupation for those who earn >50K in India.
+    top_IN_occupation = df[(df['native-country'] == 'India') & query2] \
+                                    ['occupation'].value_counts().index[0]
 
-    # DO NOT MODIFY BELOW THIS LINE
+    # Lines of code below this must not be modified
 
     if print_data:
         print("Number of each race:\n", race_count) 
