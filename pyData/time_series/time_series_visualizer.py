c904b120ba83d7bc027c8e2528f1fb5ef5cf94a7
Carolus11145 <blumkarl221@gmail.com>
2024-09-21 17:15:29 +0200

    Made final changes to visuliser

--- pyData/time_series/time_series_visualizer.py
+++ pyData/time_series/time_series_visualizer.py
@@ -18,7 +18,7 @@ def draw_line_plot():
     fig, ax = plt.subplots(figsize=(32, 10), dpi=100)
     # Set the title as well as the axes to the correct names
     ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
-    ax.set_xlabel('Data')
+    ax.set_xlabel('Date')
     ax.set_ylabel('Page Views')
     # Draw the line plot of the data frame
     sns.lineplot(data=df, legend=False)
@@ -31,27 +31,18 @@ def draw_line_plot():
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
     df_bar = df.copy()
-    df_bar['Years'] = df_bar.index.year
-    df_bar['Months'] = df_bar.index.month_name()
-    df_bar = pd.DataFrame(df_bar.groupby(['Years', 'Months'], sort=False)[
-                          'value'].mean().round().astype(int))
-    df_bar = df_bar.rename(columns={'value': 'Average Page Views'})
-    df_bar = df_bar.reset_index()
-    missing_data = {
-        'Years': [2016, 2016, 2016, 2016],
-        'Months': ['January', 'February', 'March', 'April'],
-        'Average Page Views': [0, 0, 0, 0]
-    }
-
-    df_bar = pd.concat([pd.DataFrame(missing_data), df_bar])
+    df_bar['year'] = pd.DatetimeIndex(df_bar.index).year
+    df_bar['month'] = pd.DatetimeIndex(df_bar.index).month
+    df_bar = df_bar.groupby(['year', 'month'])['value'].mean().unstack()
+    mnths = ['January', 'February', 'March', 'April', 'May', 'June',
+             'July', 'August', 'September', 'October', 'November', 'December']
     # Draw bar plot using matplotlib and seaborn methods
-    fig, ax = plt.subplots(figsize=(19.2, 10.8), dpi=100)
-    ax.set_title('Daily freeCodeCamp Forum Average Page Views per Month')
-
-    bar_chart = sns.barplot(
-        data=df_bar, x='Years', y='Average Page Views', hue='Months', palette='tab10')
-    bar_chart.set_xticklabels(bar_chart.get_xticklabels(
-    ), rotation=90, horizontalalignment='center')
+    fig = df_bar.plot(kind='bar', figsize=(14, 10)).figure
+    plt.title('Average Daily Page Views by Year and Month')
+    plt.xlabel('Years')
+    plt.ylabel('Average Page Views')
+    lgnd = plt.legend(title='Months', fontsize=12, labels=mnths)
+    title = lgnd.get_title()
 
     # Save image and return fig
     fig.savefig('bar_plot.png')
@@ -79,7 +70,7 @@ def draw_box_plot():
              'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     sns.boxplot(data=df_box, x='month', y='value', order=mnths, ax=axes[1])
     axes[1].set_title('Month-wise Box Plot (Seasonality)')
-    axes[1].set_xlabel('Months')
+    axes[1].set_xlabel('Month')
     axes[1].set_ylabel('Page Views')
 
     # Save image and return fig (don't change this part)
