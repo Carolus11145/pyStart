6e5768bf3743ef9d98a4cece96e29f7f824ad696
Carolus11145 <blumkarl221@gmail.com>
2024-10-08 12:44:37 +0200

    Completed player function

--- pyMch/rps/RPS.py
+++ pyMch/rps/RPS.py
@@ -1,10 +1,39 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+# The following function wil be used to keep track of the opponent's history and plays whatever the opponent played two plays ago..
+
+fin = {}
 
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    global fin
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    i = 5
+
+    if prev_play in ["R", "P", "S"]:
+
+        opponent_history.append(prev_play)
+
+    guess = "R" # The default until the logic begins to run
+
+    if len(opponent_history) > i:
+        input = "".join(opponent_history[-i:])
+        
+        if "".join(opponent_history[-(i+1):]) in fin.keys():
+            fin["".join(opponent_history[-(i+1):])] += 1
+        else:
+            fin["".join(opponent_history[-(i+1):])] = 1
+
+        output = [input+"R", input+"P", input+"S"]
+
+        for n in output:
+            if not n in fin.keys():
+                fin[n] = 0
+
+        est = max(output, key = lambda key: fin[key])
+
+        if est[-1] == "P":
+            guess = "S"
+        if est[-1] == "R":
+            guess = "P"
+        if est[-1] == "S":
+            guess = "R"
 
     return guess
